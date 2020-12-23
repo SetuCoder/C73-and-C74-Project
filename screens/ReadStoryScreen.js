730c1012604192dc948eecdac34a3af9db3da23d
SetuCoder <setu876301@gmail.com>
2020-12-23 19:26:23 +0530

    idk

--- screens/ReadStoryScreen.js
+++ screens/ReadStoryScreen.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 
 export default class ReadStoryScreen extends React.Component {
     render() {
       return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{textAlign:'center', fontSize:30, fontWeight: "bold"}}>Coming Soon...</Text>
         </View>
       );
     }
